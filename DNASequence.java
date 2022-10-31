5897aa07dc542d41ac54771f381ceb841857ec6f
LoliHuter1889 <112480186+LoliHuter1889@users.noreply.github.com>
2022-10-31 09:32:39 +0800

    SomehowWorking.java
    I added this because i realised i shd upload the file

--- DNASequence.java
+++ DNASequence.java
@@ -1,4 +1,3 @@
-
 // Group Name (e.g. GXTX) : 
 // 
 // Instructions  
@@ -7,13 +6,17 @@
 // 3. Do not modify the variables and methods given except for the implementation of HashCode()
 // 4. You can create additional Private variables and methods
 // 5. Do not modify any other files to execute the application
-import java.util.*;
-import java.io.*;
-import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Set;
 
 public class DNASequence {
-
     private String sequence;
+    private static HashMap<String, Integer> map = new HashMap<String, Integer>();
 
     public DNASequence(String sequence) {
         this.sequence = sequence;
@@ -27,84 +30,83 @@ public class DNASequence {
         return sequence;
     }
 
-    // Start Of Implementation Here:
-
-    // private static String dataFile = "data10000.txt";
-    // private static LinkedList<String> dnaList = new LinkedList<>();
-
-    // //readData from the data10000txt file, add inside a new hashMap, remove the
-    // sequence entry
-    // public static void readData(String sequence) {
-    // try (Scanner sc = new Scanner(new File(dataFile))) {
-    // while (sc.hasNext()) {
-    // //dnaList.add(new DNASequence(sc.next())); //populate the SLL with all the
-    // sequences from txt
-    // dnaList.add(sc.next());
-    // }
-    // } catch (FileNotFoundException ex) {
-    // System.out.println("Data File not found");
-    // }
-    // }
-    // only modify HashCode!
-
-    // all methods static; private constructor.
-
-
-    /**
-     * Generates 32 bit hash from byte array of the given length and
-     * seed.
-     * 
-     *  data   byte array to hash
-     *  length length of the array to hash
-     *  seed   initial seed value
-     * return 32 bit hash of the given array
-     */
-    public static int hash32(final byte[] data, int length, int seed) {
-        // 'm' and 'r' are mixing constants generated offline.
-        // They're not really 'magic', they just happen to work well.
-        final int m = 0x5bd1e995;
-        final int r = 24;
-
-        // Initialize the hash to a random value
-        int h = seed ^ length;
-        int length4 = length / 4;
-
-        for (int i = 0; i < length4; i++) {
-            final int i4 = i * 4;
-            int k = (data[i4 + 0] & 0xff) + ((data[i4 + 1] & 0xff) << 8)
-                    + ((data[i4 + 2] & 0xff) << 16) + ((data[i4 + 3] & 0xff) << 24);
-            k *= m;
-            k ^= k >>> r;
-            k *= m;
-            h *= m;
-            h ^= k;
+    private String getValue(char c) {
+        switch (c) {
+            case 'A':
+                return "00";
+            case 'C':
+                return "01";
+            case 'G':
+                return "10";
+            case 'T':
+                return "11";
+            default:
+                return "";
         }
-
-        // Handle the last few bytes of the input array
-        switch (length % 4) {
-            case 3:
-                h ^= (data[(length & ~3) + 2] & 0xff) << 16;
-            case 2:
-                h ^= (data[(length & ~3) + 1] & 0xff) << 8;
-            case 1:
-                h ^= (data[length & ~3] & 0xff);
-                h *= m;
-        }
-
-        h ^= h >>> 13;
-        h *= m;
-        h ^= h >>> 15;
-
-        return h;
     }
 
-   
     public int hashCode() {
-        byte[] byteArr = sequence.getBytes();
-        
-        return hash32(byteArr, 8, 10007);
-        //123123
-    
-        //return 0;
+        if (map.containsKey(sequence))
+            return map.get(sequence);
+
+        StringBuilder sequenceAsBinaryBuilder = new StringBuilder();
+        int c = 1;
+        for (char DNAChar : sequence.toCharArray()) {
+            switch (DNAChar) {
+                case 'A':
+                    sequenceAsBinaryBuilder.append("00");
+                    break;
+                case 'C':
+                    sequenceAsBinaryBuilder.append("01");
+                    break;
+                case 'G':
+                    sequenceAsBinaryBuilder.append("10");
+                    break;
+                case 'T':
+                    sequenceAsBinaryBuilder.append("11");
+                    break;
+                default:
+                    break;
+            }
+            if (c % 8 == 0)
+                sequenceAsBinaryBuilder.append(" ");
+            c++;
+        }
+
+        String[] splitted = sequenceAsBinaryBuilder.toString().split(" ");
+        int hash = Integer.parseInt(splitted[0], 2);
+
+        Set<Entry<String, Integer>> enntries = map.entrySet();
+        for (Entry<String, Integer> entry : enntries) {
+            if (countDif(sequence, entry.getKey()) <= 5)
+                return entry.getValue();
+        }
+
+        map.put(sequence, hash);
+        return hash;
     }
+
+    private int countDif(String myString, String myString1) {
+        int i = 0;
+        int count = 0;
+        int length = myString.length();
+        while (i < length) {
+            if (myString.charAt(i) != myString1.charAt(i)) {
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+
+    private List<String> shingle(String text, int k) {
+        List<String> list = new ArrayList<String>();
+
+        for (int i = 0; i < text.length() - k; i++) {
+            list.add(text.substring(i, i + k));
+        }
+
+        return list;
+    }
+
 }
